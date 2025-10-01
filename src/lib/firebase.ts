9e25a397780c5d3afd42d6760bfb0245c6cb3bec
Mahar Muhammad Saad (Asjad) <noorasjad3@gmail.com>
2025-10-01 13:45:19 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "tidyscapes-digital",
-  "appId": "1:122387321115:web:d4e7bea5493a8e4ac683be",
-  "storageBucket": "tidyscapes-digital.firebasestorage.app",
-  "apiKey": "AIzaSyCcCs2cWNYiPaanSJXb72nMNXxO_vq6fk4",
-  "authDomain": "tidyscapes-digital.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "122387321115"
+  apiKey: "AIzaSyCcCs2cWNYiPaanSJXb72nMNXxO_vq6fk4",
+  authDomain: "tidyscapes-digital.firebaseapp.com",
+  projectId: "tidyscapes-digital",
+  storageBucket: "tidyscapes-digital.firebasestorage.app",
+  messagingSenderId: "122387321115",
+  appId: "1:122387321115:web:d4e7bea5493a8e4ac683be"
 };
 
 // Initialize Firebase
